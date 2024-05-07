527e06674f2773a2cf55ac7b38ad53f634a90293
Nina <morville5audrey@gmail.com>
2024-05-07 22:08:17 +0200

    add router

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fetch-api2/',
+  /*
   server: {
     port: 3005,
   },
-    
+   */ 
   
 });
